c0e10443d05aa9c44107d3133718b3013ab8927e
WS061 <hyt@e-tominaga.com>
2016-12-08 17:25:36 +0900

    Write setup script

---
+++ setup.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+from setuptools import setup, find_packages
+from consoleADL import __author__, __version__, __doc__
+
+with open("LICENSE","r") as fp:
+    license_message = fp.read()
+
+setup(
+    name="consoleADL",
+    author=__author__,
+    description=__doc__.splitlines()[0],
+    version=__version__,
+    license=license_message,
+    packages=find_packages(),
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: Japanese",
+        "Natural Language :: English",
+        "Operating System :: Microsoft :: Windows",
+        "Operating System :: Microsoft :: Windows :: Windows 7",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 2 :: Only",
+        "Topic :: Utilities"
+    ]
+)
