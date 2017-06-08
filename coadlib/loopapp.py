a2eb0451473e02d6fc3c66ded6dee53d614dddc5
WS061 <hyt@e-tominaga.com>
2017-06-08 09:36:44 +0900

    Loop application uses the decorator.

--- coadlib/loopapp.py
+++ coadlib/loopapp.py
@@ -6,24 +6,31 @@ from .interactiveapp import InteractiveApplication, ENCODING
 class InteractiveLoopApplication(InteractiveApplication):
 
     def __init__(self, name, desc, version,
-                    padding, margin, encoding=ENCODING):
+                    padding, margin, prefix, encoding=ENCODING):
 
         super(InteractiveLoopApplication, self).__init__(
-            name, desc, version, padding, margin, "", encoding)
+            name, desc, version, padding, margin, prefix, encoding)
 
-        
-        # loop flags
-        self.LOOP_CONTINUE = 1
-        self.LOOP_BREAK = 0
+        # loop status
+        self.STATUS_EXIT = 0
+        self.STATUS_CONTINUE = 1
 
-    def loop(self, func, input_message):
 
-        loop_flag = self.LOOP_CONTINUE
+    def loop(self, func):
 
-        while loop_flag == self.LOOP_CONTINUE:
+        def mainloop():
 
-            data = self.input_console(input_message, None, validate=False)
-            loop_flag = func(data)
+            loop_flag = self.STATUS_CONTINUE
 
-        self.exit(0)
+            while loop_flag == self.STATUS_CONTINUE:
 
+                try:
+                    loop_flag = func()
+
+                except KeyboardInterrupt:
+                    self.write_error("Terminated.")
+                    self.exit(0)
+
+            self.exit(0)
+
+        return mainloop
---
+++ examples/calc.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+from coadlib.apps import InteractiveLoopApplication
+
+app = InteractiveLoopApplication(
+    name="calcuation program",
+    desc="please input number, and return total.",
+    version="1.0", padding=4, margin=3, prefix=" > "
+)
+
+app.total = 0
+
+
+@app.loop
+def main():
+
+    response = app.input_console("number", int, validate=False)
+
+    if response == "":
+        app.write("total: {:,}".format(app.total))
+        return app.STATUS_EXIT
+
+    else:
+        try:
+            app.total += int(response)
+            return app.STATUS_CONTINUE
+
+        except:
+            app.write_error("Error: incorrect data.")
+            return app.STATUS_CONTINUE
+
+
+if __name__ == "__main__":
+
+    app.write_usage()
+    main()
